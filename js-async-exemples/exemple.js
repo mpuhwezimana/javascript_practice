dc98cb63d688f4df17c60d69e4e755839b8f562b
mpuhwezimana <mpuhwearsene.com>
2017-02-24 11:21:27 +0200

    worked on setTimeout in javascript

---
+++ js-async-exemples/exemple.js
@@ -0,0 +1,15 @@
+function firstcommand() {
+  console.log("Arsene")
+}
+
+function secondcommand() {
+  console.log("Mpuhwezimana")
+}
+
+function thirdcommand() {
+  console.log("Is a real man")
+}
+
+setTimeout(firstcommand, 2000)
+setTimeout(secondcommand, 4000)
+setTimeout(thirdcommand, 6000)
